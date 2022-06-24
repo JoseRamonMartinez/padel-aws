36c70bb0972f9d9913a955109831c358bdf319c7
joseramonmartinez <josera.martinez@hotmail.com>
2022-06-24 15:44:53 +0200

    contains for get player by nade

--- lambdas/get_player/get_player_controller.py
+++ lambdas/get_player/get_player_controller.py
@@ -24,10 +24,13 @@ dybamodb_players_table = dynamodb.Table(players_table)
 
 def get_player_by_name(data):
     try:
-        response = dybamodb_players_table.query(
-            KeyConditionExpression=Key('name').eq(data["name"])
-        )
-        return response["Items"]
+        # response = dybamodb_players_table.query(
+        #     KeyConditionExpression=Key('name').eq(data["name"])
+        # )
+        # get response from dynamodb for player with name contains data["name"] using scan
+        response = dybamodb_players_table.scan()
+        response = [x for x in response["Items"] if data["name"] in x['name']]
+        return response[0]
     except Exception as e:
         raise HTTPError(500, 'Internal Error: %s' % e)
 
